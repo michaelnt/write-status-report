ab97cfb5acbe2e512f0d9115afbc7e2a55e064dc
Michael Thompson <michaelnt@gmail.com>
2014-04-07 08:09:35 +0100

    Add initial form

--- writestatusreports.js
+++ writestatusreports.js
@@ -1,15 +1,19 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to writestatusreports.";
-  };
+Accounts.config({ restrictCreationByEmailDomain: 's3group.com',
+		  sendVerificationEmail: true});
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
+    Template.writestatus.week = function () {
+	d = new Date();
+	d.setHours(0,0,0);
+	// Set to nearest Thursday: current date + 4 - current day number
+	// Make Sunday's day number 7
+	d.setDate(d.getDate() + 4 - (d.getDay()||7));
+	// Get first day of year
+	var yearStart = new Date(d.getFullYear(),0,1);
+	// Calculate full weeks to nearest Thursday
+	var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+	return weekNo;
+    };
 }
 
 if (Meteor.isServer) {
